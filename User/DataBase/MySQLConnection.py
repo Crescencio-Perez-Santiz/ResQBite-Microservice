78a3195dfe7a09c9104a4fe8c71ccf23f31a27d7
Crescens-Henry <crescencio.ps.code@gmail.com>
2024-06-09 14:57:56 -0600

    Update: Update MySQLConnection.py to create the database if it doesn't exist

--- User/DataBase/MySQLConnection.py
+++ User/DataBase/MySQLConnection.py
@@ -2,23 +2,26 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-from flask.cli import load_dotenv
+import pymysql
+from dotenv import load_dotenv
 
 Base = declarative_base()
-
 
 class DBConnection:
     def __init__(self):
         load_dotenv()
-
         host = os.getenv('DB.HOST_MYSQL')
         port = os.getenv('DB.PORT_MYSQL')
         user = os.getenv('DB.USER_MYSQL')
         password = os.getenv('DB.PASSWORD_MYSQL')
         database = os.getenv('DB.DATABASE_MYSQL')
-
         try:
+            conn = pymysql.connect(host=host, user=user, password=password)
+            cursor = conn.cursor()
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
+            conn.commit()
+            cursor.close()
+
             self.engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}')
             Base.metadata.create_all(self.engine)
             self.Session = sessionmaker(bind=self.engine)
@@ -26,9 +29,3 @@ class DBConnection:
         except Exception as e:
             print(f"Error al conectar a la base de datos: {str(e)}")
             self.Session = None
-
-    def get_session(self):
-        if self.Session is not None:
-            return self.Session()
-        else:
-            raise Exception("No se pudo establecer la conexión a la base de datos.")
