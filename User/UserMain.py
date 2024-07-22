3fff91600436e931eb370c04b24e68565d74a59f
Crescens-Henry <crescencio.ps.code@gmail.com>
2024-07-21 18:29:18 -0600

    chore: Add SSL support for UserMain.py and Dockerfile

--- User/UserMain.py
+++ User/UserMain.py
@@ -2,6 +2,7 @@ from flask import Flask
 from Infrastructure.Routes.Router import initialize_user_router
 from Infrastructure.Repositories.UserRepository import UserRepository
 from Infrastructure.Services.Dependecies import init_rabbitmq
+import ssl
 
 app = Flask(__name__)
 
@@ -9,4 +10,7 @@ init_rabbitmq()
 initialize_user_router(app, UserRepository())
 
 if __name__ == "__main__":
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain(certfile='fullchain.pem', keyfile='privkey.pem')
+
     app.run("0.0.0.0", 5000, debug=True, load_dotenv=True)
