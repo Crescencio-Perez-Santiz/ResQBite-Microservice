f449ae3b95162562b516ddc2589a4163819d2126
Crescens-Henry <crescencio.ps.code@gmail.com>
2024-07-22 19:43:43 -0600

    refactor: Add SSL support to Store application

--- Store/storeMain.py
+++ Store/storeMain.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from Infrastructure.Routes.Router import initialize_routes
 from Infrastructure.Respositories.StoreRepository import StoreRepository
-
+import ssl
 
 app = Flask(__name__)
 
 initialize_routes(app, StoreRepository())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True, load_dotenv=True)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain(certfile='fullchain.pem', keyfile='privkey.pem')
+    app.run(host='0.0.0.0', port=5001, ssl_context=context,
+            debug=True, load_dotenv=True)
